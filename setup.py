d9796d52d83ee1a1d4412ea330b97cd4b5a8fce4
L. B. Rollik <L.B.Rollik@protonmail.com>
2025-03-20 10:30:34 +0000

    "Upgrade: 1.0.0 → 1.0.1.dev0"

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, "LICENSE")) as f:
 
 setup(
     name="serial_weighing_scale",
-    version="1.0.0",
+    version="1.0.1.dev0",
     description="serial_weighing_scale",
     long_description=long_description,
     long_description_content_type="text/markdown",
